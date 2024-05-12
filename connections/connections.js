7a253ec279c71e7512990aac739ce943b615986a
Nick Sprague <n.sprague18@gmail.com>
2024-05-12 17:05:48 -0400

    finished connections and readme

--- connections/connections.js
+++ connections/connections.js
@@ -5,7 +5,8 @@ let db = mysql.createConnection(
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        database: process.env.DB_DATABASE,
+        port: 3001
     },
     console.log('You are now connected to the Employee Tracker Jacker application!🐝')
 );
